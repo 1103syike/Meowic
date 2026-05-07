6d67b2e6b1dfe4f44a33fd39b08af0390f861295
AD\dandy.cheng <dandy.cheng@pollex.com.tw>
2026-05-07 11:28:07 +0800

    feat(router): 新增路由設定。

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        data: { pageName: '首頁' },
+        loadComponent: () => import('./view/home/home').then((m) => m.Home),
+      },
+      {
+        path: 'album/:id',
+        data: { pageName: '專輯' },
+        loadComponent: () => import('./view/album/album').then((m) => m.Album),
+      },
+      {
+        path: 'song/:id',
+        data: { pageName: '歌曲' },
+        loadComponent: () => import('./view/song/song').then((m) => m.Song),
+      },
+    ],
+  },
+];
