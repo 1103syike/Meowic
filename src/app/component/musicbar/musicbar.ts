78ae25f645ce6a4738a09e7fc90c3a95fba78983
AD\dandy.cheng <dandy.cheng@pollex.com.tw>
2026-05-07 16:43:00 +0800

    feat(musicbar): 新增未登入UI提示。

--- src/app/component/musicbar/musicbar.ts
+++ src/app/component/musicbar/musicbar.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { AuthService } from '../../@service/auth.service';
+import { Login } from '../login/login';
 
 @Component({
   selector: 'app-musicbar',
-  imports: [MatIconModule],
+  imports: [MatIconModule, Login],
   templateUrl: './musicbar.html',
   styleUrl: './musicbar.scss',
-    host: {
-    'class': 'musicbar' //
-  }
+  host: {
+    class: 'musicbar', //
+  },
 })
-export class Musicbar {}
+export class Musicbar {
+  public auth: AuthService = inject(AuthService);
+  public isLoginDialogOpen = signal(false);
+
+  openLoginDialog() {
+    this.isLoginDialogOpen.set(true);
+  }
+  ngOnInit() {}
+}
