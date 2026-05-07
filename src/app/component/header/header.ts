739822ca80f92891ffb607f540b3552c02545175
AD\dandy.cheng <dandy.cheng@pollex.com.tw>
2026-05-07 11:24:17 +0800

    feat(header): 頁首切版

---
+++ src/app/component/header/header.ts
@@ -0,0 +1,20 @@
+import { Component,  signal, inject } from '@angular/core';
+import { AuthService } from '../../@service/auth.service';
+import { Login } from '../login/login';
+
+@Component({
+  selector: 'app-header',
+  imports: [Login],
+  templateUrl: './header.html',
+  styleUrl: './header.scss',
+})
+export class Header {
+  public auth: AuthService = inject(AuthService);
+  public isLoggin = this.auth.isLoggedIn()
+  public userInfo = this.auth.getUserInfo()
+  public isLoginDialogOpen = signal(false);
+
+  openLoginDialog() {
+    this.isLoginDialogOpen.set(true);
+  }
+}
