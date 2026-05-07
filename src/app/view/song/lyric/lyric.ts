9387e3b77fe2feebfbf057d1afa9415e72d98a7c
AD\dandy.cheng <dandy.cheng@pollex.com.tw>
2026-05-07 16:40:50 +0800

    feat(lyric): 歌詞頁切版。

--- src/app/view/song/lyric/lyric.ts
+++ src/app/view/song/lyric/lyric.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core';
+import { SongType } from '../../../@service/api.service';
+import { MatIcon } from "@angular/material/icon";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-lyric',
-  imports: [],
+  imports: [MatIcon,RouterLink],
   templateUrl: './lyric.html',
   styleUrl: './lyric.scss',
 })
-export class Lyric {}
+export class Lyric {
+  @Input() passedCurrentSong = signal<SongType | null>(null);
+  /////////////////////////////////////////////
+  public isShowCover = signal<boolean>(true);
+  /////////////////////////////////////////////
+
+  ngOnInit() {
+
+  }
+  showCover(boolean: boolean) {
+    this.isShowCover.set(boolean);
+  }
+}
