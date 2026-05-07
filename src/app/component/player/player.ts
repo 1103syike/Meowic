4b05d0a564357b1a06f39569e403e96d306a2809
AD\dandy.cheng <dandy.cheng@pollex.com.tw>
2026-05-07 11:28:29 +0800

    feat(player): 播放器切版

---
+++ src/app/component/player/player.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatIcon } from "@angular/material/icon";
+
+@Component({
+  selector: 'app-player',
+  imports: [MatIcon],
+  templateUrl: './player.html',
+  styleUrl: './player.scss',
+})
+export class Player {}
